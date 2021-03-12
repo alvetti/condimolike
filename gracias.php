9d7f9b1d02314b05992ffe89afd625ee3a3dd471
Alan Alvetti <alan.alvetti@coalitiontechnologies.com>
2021-03-12 10:23:13 -0300

    All done and send to review

---
+++ gracias.php
@@ -0,0 +1,8 @@
+<?php include('index.html') ?>
+<div style="position:fixed; width:100%; height:100%; text-align: :center; background:url(https://condimo.com/assets/images/bg.png);top:0; left:0; z-index:30000">
+
+	<h3 style="text-align:center; color:#fff; margin:20% 20% 20px 20%; font-size: 20pt;"> Gracias, recibimos tu solicitud</h3><br>
+	<div style="text-align:center; color:#fff; margin:auto 20% 20px 20%; font-size: 18pt;">En breve nos comunicaremos con vos. </div>
+</div>
+
+<META http-equiv="refresh" content="6;URL=https://condimo.com/condimolike">
